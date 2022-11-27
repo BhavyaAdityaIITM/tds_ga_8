759ca21cfa61a0d1584937e1fd89ca42a7b8a36f
BhavyaAdityaIITM <119077077+BhavyaAdityaIITM@users.noreply.github.com>
2022-11-27 23:18:21 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,1 +1,22 @@
+import streamlit as st
+import pickle
+import pandas as pd
 
+st.write('Program For Multiplication of 2 Numbers')
+
+st.header('User Input Parameters')
+
+def user_input():
+  num1 = st.number_input('Enter the 1st number')
+  num2 = st.number_input('Enter the 2nd number')
+  
+  inp = [num1, num2]
+  ans = num1*num2
+  
+  return ans
+
+df = user_input()
+st.write(df)
+
+#ans = df[0]*df[1
+         
